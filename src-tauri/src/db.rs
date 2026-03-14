1eecd89fa471395c60423e6331229657fe8f2ff0
surue <sachetghimire3@gmail.com>
2026-03-14 21:57:55 +0545

    Progress.

--- src-tauri/src/db.rs
+++ src-tauri/src/db.rs
@@ -6,8 +6,12 @@ use std::path::PathBuf;
 pub fn get_db_path() -> PathBuf{
     let mut path = dirs::data_dir()
         .unwrap_or_else(|| PathBuf::from("."));
-    path.push("water_reminder");
+    path.push("water_reminder")
     std::fs::create_dir_all(&path).ok();
     path.push("water.db");
     path
+}
+
+pub fn init_db() -> Result<Connection> {
+    let path = get_db_path
 }
